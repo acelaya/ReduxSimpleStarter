02c9ef6c0c710689656d1d12edc9be2f6ca768e1
Alejandro Celaya <alejandro.celaya@adidas.com>
2018-05-24 16:49:52 +0200

    Added validation to the form

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -3,13 +3,18 @@ import { Field, reduxForm } from 'redux-form';
 
 class CreatePost extends React.Component {
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <h3>Create post</h3>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field name="title" component={CreatePost.renderField} label="Title" />
           <Field name="tags" component={CreatePost.renderField} label="Tags" />
           <Field name="content" component={CreatePost.renderField} label="Content" />
+          <div className="text-right">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </div>
         </form>
       </div>
     );
@@ -20,11 +25,30 @@ class CreatePost extends React.Component {
       <div className="form-group">
         <label>{field.label}:</label>
         <input className="form-control" type="text" {...field.input} />
+        {field.meta.touched && field.meta.error && <span>{field.meta.error}</span>}
       </div>
     );
   }
+
+  onSubmit(values) {
+    console.log(values);
+  }
 }
 
+const validate = (values) => {
+  let errors = {};
+
+  if (! values.title) {
+    errors.title = 'Enter title';
+  }
+  if (! values.tags) {
+    errors.tags = 'Enter tags';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
+  validate,
   form: 'CreatePostForm'
 })(CreatePost)
