554ea4146d10e014177754070e9a64ba17141d38
Alejandro Celaya <alejandro.celaya@adidas.com>
2018-05-24 16:26:36 +0200

    Added post creation form

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class CreatePost extends React.Component {
+class CreatePost extends React.Component {
   render() {
     return (
       <div>
         <h3>Create post</h3>
+        <form>
+          <Field name="title" component={CreatePost.renderField} label="Title" />
+          <Field name="tags" component={CreatePost.renderField} label="Tags" />
+          <Field name="content" component={CreatePost.renderField} label="Content" />
+        </form>
+      </div>
+    );
+  }
+
+  static renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}:</label>
+        <input className="form-control" type="text" {...field.input} />
       </div>
     );
   }
 }
+
+export default reduxForm({
+  form: 'CreatePostForm'
+})(CreatePost)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './posts-reducer';
+import postsReducer from './posts-reducer';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: postsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
